b57993429549d94b30a2c14752ebe6c9a936eae6
Marcel Hasselaar <marcel@hasselaar.nu>
2015-10-03 09:25:11 +0200

    Initial Commit

---
+++ AVCaptureDoctor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVCaptureDoctor
+//
+//  Created by Marcel Hasselaar on 03/10/15.
+//  Copyright © 2015 AgiDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
